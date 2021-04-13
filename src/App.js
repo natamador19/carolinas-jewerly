315bb748bfcb878b6d6320a2018600f7e3346a64
natamador19 <73267858+natamador19@users.noreply.github.com>
2021-04-13 00:00:08 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
 import NavBar from "./components/NavBar/NavBar";
 import React from "react";
-import Cards from "./components/Cards/Cards";
-import logo from './logo.png';
-import CardsHolders from "./components/Cards/CardsHolder";
 import Footer from "./components/Footer/Footer";
+import Inicio from "./components/Pages/Inicio";
+import { BrowserRouter as Router, Switch,Route} from "react-router-dom";
+import Consejos from "./components/Pages/Consejos/Consejos";
+import Maquillaje from "./components/Pages/Maquillaje/Maquillaje";
+import Cabello from "./components/Pages/Cabello/Cabello";
+import CuidadoPersonal from "./components/Pages/CuidadoPersonal/cuidadopersonal";
+import Temporada from "./components/Pages/Temporada/Temporada";
+import Natural from "./components/Pages/Natural/Natural";
+import Suscribirse from "./components/Pages/Suscribir/Suscribirse";
+import Encuesta from "./components/Pages/Encuesta/Encuesta";
+
 function App() {
   return (
+    <Router>
     <section>
-    <NavBar></NavBar>
-    <CardsHolders>
-    <Cards title="Hola" p="Las descripciones de producto son textos que explican las características y características de los productos. " img={logo}></Cards>
-    <Cards title="Hola" p="Las descripciones de producto son textos que explican las características y características de los productos. " img={logo}></Cards>
-    <Cards title="Hola" p="Las descripciones de producto son textos que explican las características y características de los productos. " img={logo}></Cards>
-    <Cards title="Hola" p="Las descripciones de producto son textos que explican las características y características de los productos. " img={logo}></Cards>
-    </CardsHolders>
+         <NavBar></NavBar>
+         <Switch>
+           <Route path="/" exact>
+              <Inicio />
+           </Route>
+           <Route path="/consejos" exact>
+            <Consejos/>
+           </Route>
+           <Route path="/maquillaje" exact>
+          <Maquillaje></Maquillaje>
+        </Route>
+        <Route path="/cabello" exact>
+          <Cabello></Cabello>
+        </Route>
+        <Route path="/cuidado" exact>
+        <CuidadoPersonal></CuidadoPersonal>
+        </Route>
+        <Route path="/temporada" exact>
+          <Temporada></Temporada>
+        </Route>
+        <Route path="/natural" exact>
+         <Natural></Natural>
+        </Route>
+        <Route path="/conocenos" exact>
+        
+        </Route>
+        <Route path="/consejos" exact>
+            <Consejos/>
+        </Route>
+        <Route path="/suscribirse" exact>
+         <Suscribirse/>
+        </Route>
+        <Route path="/encuesta" exact>
+         <Encuesta/>
+        </Route>
+         </Switch>
+    
+
     <Footer></Footer>
     </section>
+    </Router>
   );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
+  
   <React.StrictMode>
+     <head>
+            <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
+            <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+            </head>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
